b3cfa494a50af878c037632e904ed42b05da785d
olivierdrg <olivierdrogue@gmail.com>
2016-05-27 14:40:18 +0200

    version1.0

--- apps/header.php
+++ apps/header.php
@@ -1,3 +1,14 @@
 <?php
+
+if (isset($_SESSION['login'])) {
+
+	if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1)
+		require('views/header_admin.phtml');
+
+	else
+		require('views/header_utilisateur.phtml');
+}
+
+else
     require('views/header.phtml');
 ?>
