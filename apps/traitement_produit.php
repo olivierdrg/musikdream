b717d551f87a11bb52db9a12cc44ccd60d784ca3
cirelevuahc <eric.chauvel@neuf.fr>
2016-05-30 14:04:47 +0200

    version1.3

--- apps/traitement_produit.php
+++ apps/traitement_produit.php
@@ -7,7 +7,7 @@ if ( isset( $_POST['action'] ) ) {
         try {
             $produit = $manager->create( $_POST );
 
-            header('Location: index.php?page=admin_liste_produit');
+            header('Location: index.php?page=admin_produits');
             exit;
         }
 
@@ -17,11 +17,24 @@ if ( isset( $_POST['action'] ) ) {
     }
 
     if ( $_POST['action'] == 'modif' ) {
-        $manager = new ProduitManager( $link );// $link => $this->link
+        $manager = new ProduitManager( $link );
         try {
-            $produit = $manager->login( $_POST );
+            $id =  $_POST['id'];
+            $produit = $manager->findById( $id );
 
-            header('Location: index.php?page=admin_list_produit');
+            $produit->setIdSousCategorie( $_POST['id_sous_categorie'] );
+            $produit->setReference( $_POST['reference'] );
+            $produit->setStock( $_POST['stock'] );
+            $produit->setPrixHt( $_POST['prix_ht'] );
+            $produit->setTva( $_POST['tva'] );
+            $produit->setDescription( $_POST['description'] );
+            $produit->setPhoto( $_POST['photo'] );
+            $produit->setNom( $_POST['nom'] );
+            $produit->setPoids( $_POST['poids'] );
+            $produit->setActif( $_POST['actif'] );
+
+            $produit = $manager->update( $produit );
+            header('Location: index.php?page=admin_produits');
             exit;
         }
 
@@ -31,7 +44,18 @@ if ( isset( $_POST['action'] ) ) {
     }
 
     if ( $_POST['action'] == 'supp' ) {
+        if ( isset( $_POST['id'] ) ) {
+            try {
+                $manager = new ProduitManager( $link );
+                $id =  $_POST['id'];
+                $produit = $manager->findById( $id );
+                $manager->remove( $produit );
+            }
 
+            catch (Exception $exception) {
+                $error = $exception->getMessage();
+            }            
+        }
     }    
 
     if ( $_POST['action'] == 'cacher' ) {
