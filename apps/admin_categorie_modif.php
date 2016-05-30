85630cb830db9b0988363cb5b52525b939bb5463
olivierdrg <olivierdrogue@gmail.com>
2016-05-30 14:10:42 +0200

    version1.1

--- apps/admin_categorie_modif.php
+++ apps/admin_categorie_modif.php
@@ -1,15 +1,12 @@
 <?php
-    $id  				= '';
-    $id_categorie		= '';
+    $id                 = '';
     $photo              = '';
     $nom                = '';
     $description        = '';
 
-    if ( isset( $_POST['id'] ) ) $id = $_POST['id'];
-    if ( isset( $_POST['id_categorie'] ) ) $id_categorie = $_POST['id_categorie'];
     if ( isset( $_POST['photo'] ) ) $photo = $_POST['photo']; 
     if ( isset( $_POST['nom'] ) ) $nom = $_POST['nom'];
-    if ( isset( $_POST['description'] ) ) $description = $_POST['description'];   
+    if ( isset( $_POST['description'] ) ) $description = $_POST['description'];  
 
-    require('views/admin_souscategorie_modif.phtml');
+    require('views/admin_categorie_modif.phtml');
 ?>
