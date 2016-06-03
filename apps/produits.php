76f2ae421a42c42da03ca3c875e3cfde2e0de262
olivierdrg <olivierdrogue@gmail.com>
2016-06-03 15:43:53 +0200

    version1.75

--- apps/produits.php
+++ apps/produits.php
@@ -2,7 +2,6 @@
     if ( isset( $_GET['id_sous_categorie'] ) ) {
 
         $manager = new SousCategorieManager( $link );
-
         $sous_categorie = $manager->findById( $_GET['id_sous_categorie'] );
     	$produits = $sous_categorie->getProduits();
         
