43bcf7ce8db32e26cff3ba121befe478ae6f5751
patson67 <patson@wanadoo.fr>
2016-06-07 16:07:36 +0200

    maj categorie

--- apps/traitement_categorie.php
+++ apps/traitement_categorie.php
@@ -59,55 +59,54 @@ if ( isset( $_POST['action'] ) ) {
     }
 
     if ( $_POST['action'] == 'upload' ) {
-        // if ( isset( $_POST['id'] ) ) {
 
-            $target_dir = "public/images/";
-            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-            $uploadOk = 1;
-            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-            // Check if image file is a actual image or fake image
-            // if(isset($_POST["submit"])) {
-                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-                if($check !== false) {
-                    // echo "File is an image - " . $check["mime"] . ".";
-                    $uploadOk = 1;
-                } else {
-                    $error = "File is not an image.";
-                    $uploadOk = 0;
-                }
-            // }
-            // Check if file already exists
-            if (file_exists($target_file)) {
-                $error = "Sorry, file already exists.";
+        $target_dir = "public/images/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+        // Check if image file is a actual image or fake image
+        // if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                // echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                $error = "Ce fichier n'est pas une image valide.";
                 $uploadOk = 0;
             }
+        // }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            $error = "Une image avec le même nom existe déjà sur le serveur.";
+            $uploadOk = 0;
+        }
+        else{
             // Check file size
             if ($_FILES["fileToUpload"]["size"] > 500000) {
-                $error = "Sorry, your file is too large.";
+                $error = "Taille de l'image trop grande ( > 500000).";
                 $uploadOk = 0;
             }
             // Allow certain file formats
             if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
             && $imageFileType != "gif" ) {
-                $error = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $error = "Uniquement des images de type JPG, JPEG, PNG & GIF.";
                 $uploadOk = 0;
             }
             // Check if $uploadOk is set to 0 by an error
             if ($uploadOk == 0) {
-                $error = "Sorry, your file was not uploaded.";
+                $error = "Erreur interne0.";
             // if everything is ok, try to upload file
             }
             else {
                 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                    $error = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                    $error = "L'image ". basename( $_FILES["fileToUpload"]["name"]). " a été correctement uploadé.";
                 } else {
-                    $error = "Sorry, there was an error uploading your file.";
+                    $error = "Erreur interne1.";
                 }
             }
+        }
 
 
-
-        // }
     }
 }
 
