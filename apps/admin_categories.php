e07ea5335f65bc8920538b97a32ae97ffe3926b1
cirelevuahc <eric.chauvel@neuf.fr>
2016-06-09 14:40:28 +0200

    version2.0

--- apps/admin_categories.php
+++ apps/admin_categories.php
@@ -1,8 +1,9 @@
 <?php
-	if ( isset( $_SESSION['login'] ) )
-	{
-		$manager = new CategorieManager( $link );
-		$categories = $manager->findAll();
-		require('views/admin_categories.phtml');
-	}
+	if ( isset( $_SESSION['login'] ) ) {
+        if ( isset( $_SESSION['admin'] ) && $_SESSION['admin'] == 1 ) {
+    		$manager = new CategorieManager( $link );
+    		$categories = $manager->findAll();
+    		require('views/admin_categories.phtml');
+    	}
+    }
 ?>
--- apps/admin_produits.php
+++ apps/admin_produits.php
@@ -1,9 +1,10 @@
 <?php
-	if ( isset( $_SESSION['login'] ) )
-	{
-		$manager = new ProduitManager( $link );
+    if ( isset( $_SESSION['login'] ) ) {
+        if ( isset( $_SESSION['admin'] ) && $_SESSION['admin'] == 1 ) {
+    		$manager = new ProduitManager( $link );
 
-		$produits = $manager->findAll();
-		require('views/admin_produits.phtml');
-	}
+    		$produits = $manager->findAll();
+    		require('views/admin_produits.phtml');
+    	}
+    }
 ?>
